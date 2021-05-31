fac0d6e7692e72067b8bee63bf67764f0ee851fb
Leonardo Lopes dos Santos <lameranha@gmail.com>
2021-05-31 17:24:04 -0300

    🏛  preços resposnivos

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
   return (
     <div className="w-full min-h-screen">
     <Head>
-      <title>Create Next App</title>
+      <title>Datajus</title>
       <link rel="icon" href="/favicon.ico" />
     </Head>
 
@@ -22,7 +22,72 @@ export default function Home() {
       </div>
       <div className="bg-white">
         <Services/>
-          <div className="px-4 sm:px-56 sm:w-7/12">
+        <Benefites/>
+        <div className="mb-20">
+        <div className="mt-24 text-center space-y-4">
+          <h1 className="font-medium text-3xl text-gray-700">Nossos Preços</h1>
+          <h3 className="text-xl text-gray-600">Lorem ipsum dolor sit amet consect adipisicing elit. </h3>
+        </div>
+        <div className="mt-8 space-y-10 sm:space-y-0 sm:justify-between sm:flex sm:px-40">
+        <div className="text-center  mx-12 py-2 shadow-2xl space-y-4">
+          <div>
+            <img className="mx-auto h-12" src="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/apple/285/department-store_1f3ec.png"/>
+            <h2 className="font-medium text-3xl text-gray-700 mt-2">Mensal</h2>
+            <h3 className="line-through text-xl text-gray-600 mt-2">R$1300</h3>
+          </div>
+          <div className="px-4">
+            <hr className="mt-4"/>
+            <h1 className="font-medium text-5xl text-blue-500 mt-4">R$960</h1>
+            <h2 className="text-xl text-gray-600 mt-4">por mês</h2>
+            <h2 className="text-2xl bg-green-100 px-4 py-4 rounded-3xl text-green-500 mt-4 mb-5">15% de desconto</h2>
+          </div>
+        </div>
+        <div className="text-center  mx-12 py-2 shadow-2xl space-y-4">
+          <div>
+            <img className="mx-auto h-12" src="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/apple/285/office-building_1f3e2.png"/>
+            <h2 className="font-medium text-3xl text-gray-700 mt-2">Semestral</h2>
+            <h3 className="line-through text-xl text-gray-600 mt-2">R$1300</h3>
+          </div>
+          <div className="px-4">
+            <hr className="mt-4"/>
+            <h1 className="font-medium text-5xl text-blue-500 mt-4">R$1170</h1>
+            <h2 className="text-xl text-gray-600 mt-4">por mês</h2>
+            <h2 className="text-2xl bg-green-100 px-4 py-4 rounded-3xl text-green-500 mt-4 mb-5">15% de desconto</h2>
+          </div>
+        </div>
+        <div className="text-center  mx-12 py-2 shadow-2xl space-y-4">
+          <div>
+            <img className="mx-auto h-12" src="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/apple/285/classical-building_1f3db-fe0f.png"/>
+            <h2 className="font-medium text-3xl text-gray-700 mt-2">Anual</h2>
+            <h3 className="line-through text-xl text-gray-600 mt-2">R$1300</h3>
+          </div>
+          <div className="px-4">
+            <hr className="my-4"/>
+            <h1 className="font-medium text-5xl text-blue-500 mt-4">R$935</h1>
+            <h2 className="text-xl text-gray-600 mt-4">por mês</h2>
+            <h2 className="text-2xl bg-green-100 px-4 py-4 rounded-3xl text-green-500 mt-4 mb-5">15% de desconto</h2>
+          </div>
+        </div>
+        
+
+        </div>
+      </div>
+      </div>
+      
+      
+      <div className="bg-gray-600">cadastro</div>
+    </main>
+
+
+
+  </div>
+  )
+}
+
+function Benefites() {
+
+  return (
+    <div className="px-4 sm:px-56 sm:w-7/12">
             <div className="space-y-4">
               <h1 className="text-3xl font-medium text-gray-700">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h1>
               <h3 className="text-xl text-gray-600">Lorem ipsum dolor sit amet, consectetur adipiscing elit. A est placerat at blandit turpis lacus eget.</h3>
@@ -70,12 +135,6 @@ export default function Home() {
               </div>
             </div>
           </div>
-        </div>
-    </main>
-
-
-
-  </div>
   )
 }
 
