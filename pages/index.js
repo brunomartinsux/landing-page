30682c494f82f32be4b6a7f3f9e3061d4222722f
Leonardo Lopes dos Santos <lameranha@gmail.com>
2021-05-30 23:46:03 -0300

    🏛  beneficios responsivos

--- pages/index.js
+++ pages/index.js
@@ -20,12 +20,57 @@ export default function Home() {
         <CallToActionDobra/>
         <FormDobra/>
       </div>
-      <Services/>
-      <div className="px-4">
-        <div>
-          <h1 className="text-2xl">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h1>
+      <div className="bg-white">
+        <Services/>
+          <div className="px-4 sm:px-56 sm:w-7/12">
+            <div className="space-y-4">
+              <h1 className="text-3xl font-medium text-gray-700">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h1>
+              <h3 className="text-xl text-gray-600">Lorem ipsum dolor sit amet, consectetur adipiscing elit. A est placerat at blandit turpis lacus eget.</h3>
+            </div>
+            <div className="mt-10 space-y-6">
+              <div className="flex flex-row space-x-2">
+              <div className="bg-blue-500 p-2 rounded-full">
+                <svg width="12" height="12" viewBox="0 0 10 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M8.75 1.1875L3.59375 6.34375L1.25 4" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+                </svg>
+              </div>
+              <h3 className="text-lg">Lorem ipsum dolor sit amet, consectetur.</h3>
+              </div>
+              <div className="flex flex-row space-x-2">
+              <div className="bg-blue-500 p-2 rounded-full">
+                <svg width="12" height="12" viewBox="0 0 10 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M8.75 1.1875L3.59375 6.34375L1.25 4" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+                </svg>
+              </div>
+              <h3 className="text-lg">Lorem ipsum dolor sit amet, consectetur.</h3>
+              </div>
+              <div className="flex flex-row space-x-2">
+              <div className="bg-blue-500 p-2 rounded-full">
+                <svg width="12" height="12" viewBox="0 0 10 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M8.75 1.1875L3.59375 6.34375L1.25 4" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+                </svg>
+              </div>
+              <h3 className="text-lg">Lorem ipsum dolor sit amet, consectetur.</h3>
+              </div>
+              <div className="flex flex-row space-x-2">
+              <div className="bg-blue-500 p-2 rounded-full">
+                <svg width="12" height="12" viewBox="0 0 10 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M8.75 1.1875L3.59375 6.34375L1.25 4" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+                </svg>
+              </div>
+              <h3 className="text-lg">Lorem ipsum dolor sit amet, consectetur.</h3>
+              </div>
+              <div className="flex flex-row space-x-2">
+              <div className="bg-blue-500 p-2 rounded-full">
+                <svg width="12" height="12" viewBox="0 0 10 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M8.75 1.1875L3.59375 6.34375L1.25 4" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+                </svg>
+              </div>
+              <h3 className="text-lg">Lorem ipsum dolor sit amet, consectetur.</h3>
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
     </main>
 
 
